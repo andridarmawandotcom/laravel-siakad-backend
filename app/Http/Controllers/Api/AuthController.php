50330220331c3293977f71f6980ec47c272964d1
Andri Darmawan <andri.drmawan@gmail.com>
2023-10-17 21:49:10 +0700

    api auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,5 +8,43 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-}
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required'
+        ]);
 
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'email' => ['email incorrect']
+            ]);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['password incorrect']
+            ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(
+            [
+                'jwt-token' => $token,
+                'user' => new UserResource($user),
+            ]
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logout successfully',
+        ]);
+    }
+
+}
